6a6f94fe18cf895367a80ee23498a4de3f9836ed
SaribJawad <msssarib12@gmail.com>
2025-05-17 20:55:46 +0500

    initial commit

---
+++ demo/demo.js
@@ -0,0 +1,6 @@
+const range_slider_integer = require('..')
+
+const opts = { min: 0, max: 30 }
+const rsi = range_slider_integer(opts)
+
+document.body.append(rsi)
---
+++ src/index.js
@@ -0,0 +1,25 @@
+const range = require('@saribj/range-slider-demo')
+const integer = require('@saribj/input-integer-ui-demo')
+
+module.exports = range_slider_integer
+
+function range_slider_integer (opts) {
+  const el = document.createElement('div')
+  const shadow = el.attachShadow({ mode: 'closed' })
+
+  const range_slider = range(opts, listen)
+  const input_integer = integer(opts, listen)
+
+  const output = document.createElement('div')
+  output.innerText = 0
+
+  shadow.append(range_slider, input_integer, output)
+
+  return el
+
+  function listen (message) {
+    const { type, body } = message
+    if (type === 'update') output.innerText = body
+    console.log(message)
+  }
+}
